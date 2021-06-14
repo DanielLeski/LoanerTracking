46f473fd0b2dd58067bf3b0bcf6782c860a5de5f
Daniel Leskiewicz <dleskiewicz13@gmail.com>
2021-06-14 13:29:45 -0500

    mysql

--- test.php
+++ test.php
@@ -1,16 +1,23 @@
 <?php
- 
- $filename = "/Users/smol/fun/PH_CB/"; 
+ $servername = 'localhost';
+ $username = 'username';
+ $password = 'password';
+  
+ $conn = new mysqli($servername, $username, $password)
+
+
+ $filename = "/Users/smol/fun/PH_CB/Table.Loaner.csv"; 
  $row = 1;
- echo "<html><body><table>\n\n"
- $f = fopen("{$filename}", 'r');
- while (($line = fgetcsv($f)) !== false) {
-    echo "<tr>";
-    foreach ($line as $cell) {
-       echo "<td>" . htmlspecialchars($cell) . "</td>"
-      }
-      echo "</tr>\n"
+ if (($handle = fopen('Table.Loaner.csv', 'r')) != FALSE) {
+  while (($data = fgetcsv($handle, 1000, ',')) != FALSE) {
+      $num = count($data);
+      echo "<p> $num fields in line $row: <br /></p>\n";
+		  $row++;
+		  for ($c=0; $c < $num; $c++) {
+			  echo $data[$c] . "<br />\n";
+		}
+	}
+	fclose($handle);
  }
-fclose($f);
-echo "\n</table></body></html>";
+
 ?>

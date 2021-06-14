386db8441e3eac330f519f1f3a9712d3ebe80ce8
Daniel Leskiewicz <dleskiewicz13@gmail.com>
2021-06-14 10:27:33 -0500

    resources

--- test.php
+++ test.php
@@ -1,5 +1,15 @@
 <?php
- echo <"
+ $row = 1;
+ echo "<html><body><table>\n\n"
+ $f = fopen("", 'r');
+ while (($line = fgetcsv($f)) !== false) {
+    echo "<tr>";
+    foreach ($line as $cell) {
+       echo "<td>" . htmlspecialchars($cell) . "</td>"l
 
-
+      }
+      echo "</tr>\n"
+ }
+fclose($f);
+echo "\n</table></body></html>";
 ?>

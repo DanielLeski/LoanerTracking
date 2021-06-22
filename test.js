1f9bba19608a06a08f891b34ae0da3a9ece736e4
Daniel Leskiewicz <dleskiewicz13@gmail.com>
2021-06-22 09:40:18 -0500

    am i even smart?

--- test.js
+++ test.js
@@ -3,7 +3,38 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
+const readline = require('readline')
+
+
+//Reading in the csv file
 fs.createReadStream('Table.Loaner.csv')
   .pipe(csv())
   .on('data', (data)=> results.push(data))
   .on('end', ()=> console.log(results));
+
+//Creating arrays to store the values 
+let username = [];
+let barcode = [];
+let CB_AV = [];
+let CB_OUT = [];
+var d = new Date();
+var current = d.getTime();
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var option; 
+
+rl.question("What are you trying to loan", function(answer) {
+  option = answer;
+  console.log(answer);
+  rl.close();
+  console.log(current);
+});
+
+alert(current);
+
+
+

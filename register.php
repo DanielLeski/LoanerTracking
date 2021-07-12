5667809ceb6ad0e846bc53d4559df12baefda2ce
Daniel Leskiewicz <dleskiewicz13@gmail.com>
2021-07-12 07:59:47 -0500

    access code works

--- register.php
+++ register.php
@@ -21,25 +21,32 @@ include_once('/Users/smol/fun/PHP_CB/login.php');
     echo "Unable to send. Please try again";
    }
  }
- function adding_user($user, $pass, $role) {
+ 
+ function adding_user($user, $pass, $role, $code) {
    global $conn;
-   $md5password = md5($pass); 
-   $sql = "INSERT INTO users (username, password, role) VALUES ('$user', '$md5password', '$role')";
-   $data = $conn->query($sql);
-   if ($data) {
-     echo "Your registration is complete!";
+   global $message;
+   if ($code === $GLOBALS['message']) {
+     $md5password = md5($pass); 
+     $sql = "INSERT INTO users (username, password, role) VALUES ('$user', '$md5password', '$role')";
+     $data = $conn->query($sql);
+    if ($data) {
+      echo "Your registration is complete!";
     }
   }
+ }
+  
  
  #changing a users permission
  function modify_user($user, $pass, $role) {
   global $conn;
   global $password; 
-  $m5password_updated = md5($pass);
-  $sql = "UPDATE users SET role='$role' WHERE username='$user' AND password='$m5password_updated'";
-  $data = $conn->query($sql);
-  if ($data) {
-    echo "Information is updated";
+  if ($code === $GLOBALS['message']) {
+   $m5password_updated = md5($pass);
+   $sql = "UPDATE users SET role='$role' WHERE username='$user' AND password='$m5password_updated'";
+   $data = $conn->query($sql);
+    if ($data) {
+     echo "Information is updated";
+   }
   }
- }
-?>
+}
+ ?>
